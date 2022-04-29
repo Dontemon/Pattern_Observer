1d13996222aa563d132698aa0cd0cef9f0d220a4
Vyacheslav <slavka2001@list.ru>
2022-04-29 17:16:02 +0700

    переход на QT

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-#include <fstream>
+#include <QCoreApplication>
 
-int main()
+int main(int argc, char *argv[])
 {
-	setlocale(LC_ALL, "Russian");
+    QCoreApplication a(argc, argv);
 
+    return a.exec();
 }
